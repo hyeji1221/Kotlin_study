95371aabb47cbfd4e3cb0e97529f96744530e1ae
hyeji1221 <lhg2615@naver.com>
2020-11-09 01:06:59 +0900

    chapter 02

---
+++ chapter 02/section1/UserClassImport.kt
@@ -0,0 +1,14 @@
+package chap02.section1
+
+import com.example.edu.Person as User
+
+fun main() {
+    val user1 = User("Kildong", 30) // com.example.edu의 Person이 User로 대체
+    val user2 = Person("A123", "Kildong")
+    println(user1.name)
+    println(user1.age)
+    println(user2.id)
+    println(user2.name)
+
+}
+class Person(var id: String, val name: String)
