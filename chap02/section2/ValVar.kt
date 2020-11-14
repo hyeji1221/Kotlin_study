b8940d0173e912115e4a5028ac387a84f0a212d7
hyeji1221 <lhg2615@naver.com>
2020-11-15 01:11:56 +0900

    chapter02

---
+++ chap02/section2/ValVar.kt
@@ -0,0 +1,12 @@
+package chap02.section2
+
+fun main() {
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+}
