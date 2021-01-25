5365ed18679040bb7e62ea84e77e1ea76007dd4d
Hyeji Lim <59350891+hyeji1221@users.noreply.github.com>
2021-01-25 15:40:56 +0900

    chapter06

---
+++ chap06/section3/CompanionObject.kt
@@ -0,0 +1,19 @@
+package chap06.section3
+
+class Person {
+    var id: Int = 0
+    var name: String = "Youngdeok"
+    companion object{
+        var language: String = "Korean"
+        fun work() {
+            println("working...")
+        }
+    }
+}
+
+fun main() {
+    println(Person.language)
+    Person.language = "English"
+    println(Person.language)
+    Person.work()
+}
